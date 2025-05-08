537ec9084614843beb0872e15d879d82adf9d424
John Santiago Jr. <jdsantiagojr@protonmail.com>
2025-05-08 08:08:11 -0700

    ...

--- megavul/main.py
+++ megavul/main.py
@@ -1,3 +1,6 @@
+import subprocess
+import os
+
 from megavul.pipeline.download_cve_from_nvd import crawl_nvd
 from megavul.pipeline.extract_and_download_commit import extract_and_download_commit
 from megavul.pipeline.extract_commit_diff import extract_commit_diff
@@ -6,6 +9,27 @@ from megavul.pipeline.extract_graph_and_abstract import extract_graph_and_abstra
 from megavul.pipeline.flatten_megavul import generate_megavul
 from megavul.util.logging_util import global_logger
 from megavul.util.config import crawling_language
+from megavul.util.config import crawling_language
+from megavul.util.storage import StorageLocation
+from tree_sitter import Language
+
+
+def ensure_tree_sitter_built(language_name: str):
+    tree_sitter_name = f'tree-sitter-{language_name}'
+    tree_sitter_path = StorageLocation.tree_sitter_dir() / tree_sitter_name
+    tree_sitter_so = StorageLocation.result_dir() / 'build' / f'build-{tree_sitter_name}.so'
+
+    if not tree_sitter_so.exists():
+        print("Running tree-sitter generate")
+        subprocess.call(
+            'tree-sitter generate',
+            cwd=tree_sitter_path,
+            env=os.environ.copy(), shell=True
+        )
+        print("Building tree-sitter")
+        Language.build_library(str(tree_sitter_so), [str(tree_sitter_path)])
+    else:
+        raise Exception("tree-sitter does not exists")
 
 if __name__ == '__main__':
     global_logger.info(f"MegaVul is now crawling {crawling_language}")
@@ -20,6 +44,8 @@ if __name__ == '__main__':
     extract_and_download_commit()
 
     # step.4 using tree-sitter to extract functions and clean dataset using multiple filters
+    ensure_tree_sitter_built("c")
+    ensure_tree_sitter_built("cpp")
     extract_commit_diff()
 
     # step.5 using joern to generate graph and generate abstract functions using CodeAbstracter
